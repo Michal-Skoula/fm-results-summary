b8d33c85ae7657ec23a5a7bde9e3141750642b8c
Michal <imichalthemichal@gmail.com>
2023-05-21 14:44:32 +0200

    v1

---
+++ script.js
@@ -0,0 +1,13 @@
+let reactionArea;
+let memoryArea;
+let verbalArea;
+let visualArea;
+
+function loadAreas() {
+  reactionArea = document.querySelector('.js-reaction');
+  memoryArea = document.querySelector('.js-memory');
+  verbalArea = document.querySelector('.js-verbal');
+  visualArea = document.querySelector('.js-visual');
+}
+
+  
